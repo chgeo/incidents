ac159647ff39601675a29d47dc7010ff3132f466
Christian Georgi <christian.georgi@sap.com>
2019-11-18 18:46:46 +0100

    Better code

--- srv/incidents.js
+++ srv/incidents.js
@@ -1,9 +1,13 @@
 module.exports = function (service) {
 
+  // sets default priority dependending on category
   service.before ('CREATE', 'Incidents', (req) => {
     const incident = req.data
-    if (incident.category_code == 'safety') {
-      incident.priority_code = 'high'
+    if (!incident.priority_code) {
+      switch (incident.category_code) {
+        case 'safety': return incident.priority_code = 'high'
+        default      : return incident.priority_code = 'normal'
+      }
     }
   })
 
