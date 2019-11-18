8f80ae00f6ca1b52a414cc866829584ddcca3abf
Christian Georgi <christian.georgi@sap.com>
2019-11-18 18:41:20 +0100

    Fix code

--- srv/incidents.js
+++ srv/incidents.js
@@ -3,7 +3,7 @@ module.exports = function (service) {
   service.before ('CREATE', 'Incidents', (req) => {
     const incident = req.data
     if (incident.category_code == 'safety') {
-      incident.priority == 'High'
+      incident.priority_code = 'high'
     }
   })
 
